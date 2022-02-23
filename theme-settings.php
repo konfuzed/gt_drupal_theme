a8f721187b22a2cf7431a56376ac9dc7ac380a67
Michael Sheldon <ms123@gatech.edu>
2022-02-23 16:16:02 -0500

    phpdoc

--- theme-settings.php
+++ theme-settings.php
@@ -2,11 +2,9 @@
 /**
  * Implementation of hook_form_system_theme_settings_alter()
  *
- * @param $form
- *   Nested array of form elements that comprise the form.
+ * @param array $form  Nested array of form elements that comprise the form.
  *
- * @param $form_state
- *   A keyed array containing the current state of the form.
+ * @param array $form_state  A keyed array containing the current state of the form.
  */
 function gt_form_system_theme_settings_alter(&$form, &$form_state)
 {
